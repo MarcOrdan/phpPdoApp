dae6acebc6013c17cada1fd2f0cbead98d497e15
Mac Ordan <marcordan@gmail.com>
2019-02-13 11:59:01 +0800

    added script for fetching data from mysql

--- index.php
+++ index.php
@@ -2,3 +2,23 @@
     require 'classes/Database.php';
 
     $database = new Database;
+
+    $database->query('SELECT * FROM posts');
+    /*  EXECUTE THIS IF YOU WANT TO FILTER. 
+        example of bind use case.
+    $database->query('SELECT * FROM posts WHERE id = :id');
+    $database->bind(':id', 1);
+    */
+    $rows = $database->resultset();
+?>
+
+    <h1>Blog Bois</h1>
+    <div>
+    <?php foreach($rows as $row) : ?>
+        <div>
+            <h3><?php echo $row['title']; ?></h3>
+            <p><?php echo $row['body']; ?></p>
+        </div>
+    <?php endforeach ?>
+    </div>
+    
